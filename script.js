c7e07a6cb4a1e3dc57880f7df5644cbb54ce1aa3
Trent Townsend <trent.townsend2@gmail.com>
2023-01-04 20:04:13 +1000

    Commenced function to validate on submit

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const errorIcon = document.querySelectorAll('.input-field svg')
 const inputsFields = document.querySelectorAll('input')
+const submitBtn = document.getElementById("submitBtn")
 const inputsArray = Array.from(document.querySelectorAll('input'));
+
+
+submitBtn.addEventListener("click", submitValidation)
 
 inputsFields.forEach(inputField => {
     inputField.addEventListener('blur', unselectedCheckValidity);
     inputField.addEventListener('keyup', liveCheckValidInput);
 })
 
+//Validate inputs on hitting submission button
+function submitValidation() {
+    event.preventDefault();
+
+    inputsFields.forEach(inputField => {
+        if (!inputField.checkValidity()) {
+            console.log(`Invalid input in ${inputField.id}`)
+        }
+
+    })
+}
+
+//Validate inputs when leaving an input field
 function unselectedCheckValidity() {
     if (!document.getElementById(event.target.id).checkValidity()){
         document.querySelector(`#${event.target.id} + svg`).classList.add('invalid')
@@ -15,6 +32,7 @@ function unselectedCheckValidity() {
     }
 }
 
+//Validate inputs live while typing
 function liveCheckValidInput() {
     for (const input of inputsArray) {
             if (!input.checkValidity() && document.activeElement.id == input.id) {
