6b5f30271caa183ecb70302ccf32b3214e42a6bf
Trent Townsend <trent.townsend2@gmail.com>
2023-01-04 19:11:12 +1000

    Add validity function to check function is valid when text input goes out of focus

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const errorIcon = document.querySelectorAll('.input-field svg')
-const inputs = Array.from(document.querySelectorAll('input'));
+const inputsFields = document.querySelectorAll('input')
+const inputsArray = Array.from(document.querySelectorAll('input'));
 
-function liveCheckValidInput() {
-    for (const input of inputs) {
-            if (!input.checkValidity() && document.activeElement.id == input.id) {
-                errorIcon[inputs.indexOf(input)].classList.add('invalid')
-            }
-        else if (input.checkValidity() && document.activeElement.id == input.id) {
-            errorIcon[inputs.indexOf(input)].classList.remove('invalid')
-        }
+inputsFields.forEach(inputField => {
+    inputField.addEventListener('blur', unselectedCheckValidity);
+    inputField.addEventListener('keyup', liveCheckValidInput);
+})
+
+function unselectedCheckValidity() {
+    if (!document.getElementById(event.target.id).checkValidity()){
+        document.querySelector(`#${event.target.id} + svg`).classList.add('invalid')
+    } else if (document.getElementById(event.target.id).checkValidity()){
+        document.querySelector(`#${event.target.id} + svg`).classList.remove('invalid')
     }
 }
+
+function liveCheckValidInput() {
+    for (const input of inputsArray) {
+            if (!input.checkValidity() && document.activeElement.id == input.id) {
+                errorIcon[inputsArray.indexOf(input)].classList.add('invalid')
+            }
+        else if (input.checkValidity() && document.activeElement.id == input.id) {
+            errorIcon[inputsArray.indexOf(input)].classList.remove('invalid')
+        }
+    }
+} 
