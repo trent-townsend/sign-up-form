021ef0139b4e37447a4f118c65fab66374f92d8b
Trent Townsend <trent.townsend2@gmail.com>
2023-01-04 18:07:38 +1000

    Change live validation function to only check fields that are in focus

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const errorIcon = document.querySelectorAll('.input-field svg')
 const inputs = Array.from(document.querySelectorAll('input'));
 
-console.log(inputs)
-
 function liveCheckValidInput() {
     for (const input of inputs) {
-        if (!input.checkValidity()) {
-            errorIcon[inputs.indexOf(input)].classList.add('invalid')
-        }
-        else if (input.checkValidity()) {
+            if (!input.checkValidity() && document.activeElement.id == input.id) {
+                errorIcon[inputs.indexOf(input)].classList.add('invalid')
+            }
+        else if (input.checkValidity() && document.activeElement.id == input.id) {
             errorIcon[inputs.indexOf(input)].classList.remove('invalid')
         }
     }
