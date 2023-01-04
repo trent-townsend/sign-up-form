801015a43e5b058be90c3d9bd9df2ac268e46410
Trent Townsend <trent.townsend2@gmail.com>
2023-01-04 16:08:23 +1000

    Live validation added to first name field

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const firstName = document.getElementById('first-name');;
+const errorIcon = document.querySelectorAll('.input-field svg')
+const firstName = document.getElementById('first-name');
 
 console.log(firstName)
 
+
+function liveCheckValidInput() {
+    console.log(firstName.checkValidity())
+    if (!firstName.checkValidity()) {
+        errorIcon[0].classList.add('invalid');
+    }
+    else if (firstName.checkValidity()) {
+                errorIcon[0].classList.remove('invalid');
+            }
+        }
