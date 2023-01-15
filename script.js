ea2dadb2a8c97e7560eafd24ed2a028f040505b5
Trent Townsend <trent.townsend2@gmail.com>
2023-01-15 16:01:39 +1000

    Add validation on submit and refractored live validation functions

--- script.js
+++ script.js
@@ -1,71 +1,83 @@
 const inputFields = document.querySelectorAll(".input-field");
-const errorMessageDivs = document.querySelectorAll(".error-text");
+const form = document.querySelector("form")
 
 inputFields.forEach(inputField => {
-    inputField.addEventListener('keyup', liveValidation);
-    inputField.firstElementChild.addEventListener('blur', focusValidation);
+    inputField.firstElementChild.addEventListener('input', (event) => {
+        if (event.target.validity.valid) {
+            isValid(event.target)
+        }
+        else {
+            showInvalid(event.target)
+        }
+    })
+    inputField.firstElementChild.addEventListener('blur', (event) => {
+        if (event.target.validity.valid) {
+            isValid(event.target)
+        }
+        else {
+            showInvalid(event.target)
+        }
+    })
 })
 
-function liveValidation(event) {
-    let errorText = event.target.nextElementSibling;
-    let targetID = event.target.id;
+form.addEventListener('submit', (event) => {
+    inputFields.forEach(inputField => {
+        if (!inputField.firstElementChild.validity.valid) {
+            showInvalid(inputField.firstElementChild)
+            event.preventDefault()
+        }
+    })
+})
+
+function isValid(validField) {
+    let errorText = validField.nextElementSibling;
     errorText.innerHTML = "";
-
-    if (!event.target.checkValidity()) {
-        if (targetID == "password" && event.target.value.length == 0) {
-            event.target.parentElement.classList.remove("invalid-large");
-            errorText.classList.remove("invalid-large");
-        }
-        event.target.parentElement.classList.add("invalid");
-        errorText.classList.add("invalid");
-
-        if (event.target.value.length === 0) {
-            errorText.innerHTML = `${event.target.placeholder} cannot be empty`;
-        }
-
-        else {
-            if (targetID == "first-name" || targetID == "last-name" && event.target.validity.patternMismatch) {
-                let allowedChar = /^[A-Za-z \-,`'.]+/
-                let invalidStr = event.target.value
-                let invalidChars = []
-                for (let i = 0; i < invalidStr.length; i++) {
-                    if (!allowedChar.test(invalidStr[i])) {
-                        invalidChars.push(invalidStr[i]);
-                    }
-                }
-                if (invalidChars.length == 1) {
-                    errorText.innerHTML = `Contains invalid character (${invalidChars.toString()})`;
-                }
-                else {
-                    errorText.innerHTML = `Contains invalid characters (${invalidChars.join(", ")})`;
-                }
-            }
-        
-        else if (targetID == "email-address") {
-            errorText.innerHTML = "Please enter a valid email address";
-        }
-
-        else if (targetID == "password" && event.target.validity.patternMismatch) {
-            event.target.parentElement.classList.add("invalid-large");
-            errorText.classList.add("invalid-large");
-            errorText.innerHTML = "Password must contain at least: <ul><li>8 characters</li><li>one uppercase character</li><li>one number</li><li>one special character</li></ul>"
-        }
-        
-    }
-    }
-    else if (event.target.checkValidity()) {
-        event.target.parentElement.classList.remove("invalid");
-        errorText.classList.remove("invalid-large");
-        event.target.parentElement.classList.remove("invalid-large")
-        errorText.classList.remove("invalid");
-    }
+    validField.parentElement.classList.remove("invalid");
+    errorText.classList.remove("invalid");
+    validField.parentElement.classList.remove("invalid-large")
 }
 
-function focusValidation(event) {
-    let errorText = event.target.nextElementSibling
-    if (event.target.textLength == 0) {
-        event.target.parentElement.classList.add("invalid");
-        errorText.classList.add("invalid")
-        errorText.innerHTML = `${event.target.placeholder} cannot be empty`;
+function showInvalid(invalidField) {
+    let errorText = invalidField.nextElementSibling
+    invalidField.parentElement.classList.add("invalid");
+    errorText.classList.add("invalid");
+
+    if (invalidField.validity.valueMissing) {
+        errorText.classList.remove("invalid-large");
+        invalidField.parentElement.classList.remove("invalid-large")
+        errorText.innerHTML = `${invalidField.placeholder} cannot be empty`;
     }
+    else if (invalidField.validity.patternMismatch && invalidField.id != 'password') {
+        let allowedChar = new RegExp(invalidField.pattern)
+        let invalidStr = invalidField.value
+        let invalidChars = []
+
+        for (let i = 0; i < invalidStr.length; i++) {
+            if (!allowedChar.test(invalidStr[i])) {
+                invalidChars.push(invalidStr[i]);
+            }
+        }
+        if (invalidChars.length == 1) {
+            errorText.innerHTML = `Contains invalid character (${invalidChars.toString()})`;
+        }
+        else {
+            errorText.innerHTML = `Contains invalid characters (${invalidChars.join(", ")})`;
+        }
+    }
+    else if (invalidField.validity.patternMismatch && invalidField.id == 'password') {
+        invalidField.parentElement.classList.add("invalid-large");
+        errorText.classList.add("invalid-large");
+        errorText.innerHTML = `Password must contain at least: 
+        <ul>
+            <li>8 characters</li>
+            <li>one uppercase character</li>
+            <li>one number</li>
+            <li>one special character</li>
+        </ul>`
+    }
+
+    else if (invalidField.validity.typeMismatch && invalidField.id == 'email-address') {
+        errorText.innerText = "Please enter a valid email address"
+    }
+   
 }
